54ee22bb0b5a3ffc7dfdf2750349951673e23b4f
protonn <support@argentum.io>
2016-09-05 23:56:48 -0500

    Fix block explorer transaction link from QT wallet

--- src/qt/optionsmodel.cpp
+++ src/qt/optionsmodel.cpp
@@ -66,7 +66,7 @@ void OptionsModel::Init()
     bDisplayAddresses = settings.value("bDisplayAddresses", false).toBool();
 
     if (!settings.contains("strThirdPartyTxUrls"))
-        settings.setValue("strThirdPartyTxUrls", "https://chainz.cryptoid.info/arg/%s");
+        settings.setValue("strThirdPartyTxUrls", "https://chainz.cryptoid.info/arg/tx.dws?%s");
     strThirdPartyTxUrls = settings.value("strThirdPartyTxUrls", "").toString();
 
     if (!settings.contains("fCoinControlFeatures"))
